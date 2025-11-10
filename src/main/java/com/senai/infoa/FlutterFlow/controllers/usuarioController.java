a478e142d48a57a5002c8f4423475fc42d703563
GabrielQuinelato <gabrielpontes1316@gmail.com>
2025-11-10 08:21:23 -0300

    update commit

--- src/main/java/com/senai/infoa/FlutterFlow/controllers/usuarioController.java
+++ src/main/java/com/senai/infoa/FlutterFlow/controllers/usuarioController.java
@@ -3,8 +3,8 @@ package com.senai.infoa.FlutterFlow.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senai.infoa.FlutterFlow.models.usuario;
@@ -18,16 +18,19 @@ public class usuarioController {
     private usuarioService us;
     
     @PostMapping("/salvar")
-    public usuario salvar(@RequestBody usuario usuario) {
+    public usuario salvar(@RequestParam usuario usuario) {
         return us.salvar(usuario);
     }
 
     @PostMapping("/confirmar")
-    public ResponseEntity<String> cadastrarUsuario(@RequestBody usuario usuario) {
-        if (!usuario.getSenha().equals(usuario.getConfirmarSenha())) {
-            return ResponseEntity.badRequest().body("As senhas não coincidem!");
-        }
+public ResponseEntity<String> cadastrarUsuario(
+        @RequestParam String senha,
+        @RequestParam String email
+) {
+    if (!senha.equals(senha)) {
+        return ResponseEntity.badRequest().body("As senhas não coincidem!");
+    }
 
-        return ResponseEntity.ok("Usuário cadastrado com sucesso!");
+    return ResponseEntity.ok("Usuário logado com sucesso!");
     }
 }
